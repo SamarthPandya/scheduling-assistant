3c03cb0d0601691474cdcd4cc9607805dd7b38ad
SamarthPandya <samarthpandya134@gmail.com>
2022-08-29 21:01:48 +0530

    Creating Second Tab

--- src/Components/AllTabs/SecondTab.js
+++ src/Components/AllTabs/SecondTab.js
@@ -1,6 +1,62 @@
 import React from "react";
 import "./SecondTab.css";
+import { Bar } from "react-chartjs-2";
+import { CategoryScale } from "chart.js";
+import Chart from "chart.js/auto";
+Chart.register(CategoryScale);
 const SecondTab = () => {
-  return <div className="SecondTab">Coming soonnnnn</div>;
+  // modify this data object after fetching from the database
+  const data = {
+    xlabels: ["TOC", "FA", "NA", "GT"],
+    ylabels: [15, 20, 13, 17],
+  };
+  return (
+    <div className="SecondTab">
+      <div>
+        <Bar
+          data={{
+            // Name of the variables on x-axies for each bar
+            labels: data.xlabels,
+            datasets: [
+              {
+                // Label for bars
+                label: "Attendance",
+                // Data or value of your each variable
+                data: data.ylabels,
+                // Color of each bar
+                backgroundColor: ["blue", "green", "red", "yellow"],
+                // Border color of each bar
+                borderColor: ["aqua", "green", "red", "yellow"],
+                borderWidth: 0.5,
+              },
+            ],
+          }}
+          // Height of graph
+          height={400}
+          options={{
+            maintainAspectRatio: false,
+            scales: {
+              y: {
+                Max: 44,
+              },
+              y: [
+                {
+                  ticks: {
+                    // The y-axis value will start from zero
+                    beginAtZero: true,
+                  },
+                },
+              ],
+            },
+            legend: {
+              labels: {
+                fontSize: 15,
+              },
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 export default SecondTab;
